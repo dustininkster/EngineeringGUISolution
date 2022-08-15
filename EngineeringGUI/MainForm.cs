b7c1ef314b96d0c874dc35b64af7b25412af5191
Dustin <dustininkster@gmail.com>
2022-08-14 21:07:22 -0500

    Add project files.

---
+++ EngineeringGUI/MainForm.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EngineeringGUI
+{
+    public partial class MainForm : Form
+    {
+        // Declarations of global variables/constants
+        const int MAIN_SPLITTER_DISTANCE = 250;
+        const int MAIN_PANEL2_SPLITTER_DISTANCE = 400;
+        const int MAIN_PANEL1_SPLITTER_DISTANCE = 25;
+        const int DETAILS_PROPERTIES_SPLITTER_DISTANCE = 200;
+        const int ROW_SPACING = 33;
+        const int PANEL_PADDING = 15;
+        const int FILTER_STRING_LENGTH = 5;
+
+        // Declarations/ Initializations of Fonts used
+        System.Drawing.Font fntID = new System.Drawing.Font("Courier Sans", 25, System.Drawing.FontStyle.Regular);
+        System.Drawing.Font fntSectionHeader = new System.Drawing.Font("Courier Sans", 15, System.Drawing.FontStyle.Regular);
+        System.Drawing.Font normalFont = new System.Drawing.Font("Courier New", 8, System.Drawing.FontStyle.Regular);
+
+        // Declarations/ Initializations of Arrays/Lists of controls
+        // If more than one of the same type of list is used it's stored in an array
+        System.Windows.Forms.TabControl tbSelectionTabs;
+        List<SplitContainer>[] splitContainers = new List<SplitContainer>[4];
+        List<TabPage> tabPages = new List<TabPage>();
+        List<ComboBox> comboBoxes = new List<ComboBox>();
+        List<ListBox> listBoxes = new List<ListBox>();
+        List<DataGridView>[] dataGridViews = new List<DataGridView>[2];
+        List<Label> labels = new List<Label>();
+        List<Panel> panels = new List<Panel>();
+
+        /// <summary>
+        /// Constructor for the MainForm.
+        /// </summary>
+        /// <param name="numTabsToInit">Number of Tabs to start with</param>
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //Initialize basic properties of tab control
+            tbSelectionTabs = new System.Windows.Forms.TabControl();
+            tbSelectionTabs.TabIndex = 0;
+            tbSelectionTabs.Dock = DockStyle.Fill;                              // Sets the tabs to fill the entire form
+            Controls.Add(this.tbSelectionTabs);
+
+            //Initialize array items for split containers and datagridviews
+            splitContainers[0] = new List<SplitContainer>();
+            splitContainers[1] = new List<SplitContainer>();
+            splitContainers[2] = new List<SplitContainer>();
+            splitContainers[3] = new List<SplitContainer>();
+            dataGridViews[0] = new List<DataGridView>();
+            dataGridViews[1] = new List<DataGridView>();
+
+        }
+
+        /// <summary>
+        /// Method creates a new tab to the main form and adds the common controls
+        /// </summary>
+        public void AddNewTab()
+        {
+            const int MAIN_SPLIT = 0;
+            const int MAIN_PANEL_1_SPLIT = 1;
+            const int MAIN_PANEL_2_SPLIT = 2;
+            const int PROP_SPLIT = 3;
+            const int TOP_PROPERTIES = 0;
+            const int BOTTOM_PROPERTIES = 1;
+
+            tabPages.Add(new TabPage());
+            tbSelectionTabs.Controls.Add(tabPages.Last());
+            tabPages.Last().Size = tbSelectionTabs.Size;
+            tabPages.Last().Text = "Tab " + tbSelectionTabs.TabPages.Count;
+
+            // Instantiate the element in the array
+            splitContainers[0].Add(new SplitContainer());
+            splitContainers[1].Add(new SplitContainer());
+            splitContainers[2].Add(new SplitContainer());
+            splitContainers[3].Add(new SplitContainer());
+
+            comboBoxes.Add(new ComboBox());
+            listBoxes.Add(new ListBox());
+            dataGridViews[0].Add(new DataGridView());
+            dataGridViews[1].Add(new DataGridView());
+            labels.Add(new Label());
+            panels.Add(new Panel());
+
+            // Set properties for each element
+            // Main split container divider
+            splitContainers[MAIN_SPLIT].Last().Dock = DockStyle.Fill;
+            splitContainers[MAIN_SPLIT].Last().IsSplitterFixed = true;
+            splitContainers[MAIN_SPLIT].Last().TabStop = false;
+            splitContainers[MAIN_SPLIT].Last().Orientation = Orientation.Vertical;
+            splitContainers[MAIN_SPLIT].Last().BorderStyle = BorderStyle.Fixed3D;/**/
+            splitContainers[MAIN_SPLIT].Last().FixedPanel = FixedPanel.Panel1;
+
+            // Split container between combobox and listview
+            splitContainers[MAIN_PANEL_1_SPLIT].Last().IsSplitterFixed = true;
+            splitContainers[MAIN_PANEL_1_SPLIT].Last().FixedPanel = FixedPanel.Panel1;
+            splitContainers[MAIN_PANEL_1_SPLIT].Last().TabStop = false;
+            splitContainers[MAIN_PANEL_1_SPLIT].Last().Orientation = Orientation.Horizontal;
+            splitContainers[MAIN_PANEL_1_SPLIT].Last().BorderStyle = BorderStyle.Fixed3D;
+            splitContainers[MAIN_PANEL_1_SPLIT].Last().Dock = DockStyle.Fill;
+
+            // Split container between the Main panel and the image/properties panels
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().MinimumSize = new System.Drawing.Size(10, 10);
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().Panel1MinSize = 35;
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().Panel2MinSize = 5;
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().TabStop = false;
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().Orientation = Orientation.Vertical;
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().BorderStyle = BorderStyle.Fixed3D;
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().Dock = DockStyle.Fill;
+
+            // Split container between image panel and properties panel
+            splitContainers[PROP_SPLIT].Last().Panel1MinSize = 10;
+            splitContainers[PROP_SPLIT].Last().Panel2MinSize = 5;
+            splitContainers[PROP_SPLIT].Last().TabStop = false;
+            splitContainers[PROP_SPLIT].Last().Orientation = Orientation.Horizontal;
+            splitContainers[PROP_SPLIT].Last().BorderStyle = BorderStyle.Fixed3D;
+            splitContainers[PROP_SPLIT].Last().Dock = DockStyle.Fill;
+
+            // Combobox for narrowing choices
+            comboBoxes.Last().TabIndex = 1;
+            comboBoxes.Last().Height = 5;
+            comboBoxes.Last().Dock = DockStyle.Top;
+
+            // List box for selecting the appropriate item
+            listBoxes.Last().TabIndex = 2;
+            listBoxes.Last().Font = normalFont;
+            listBoxes.Last().Anchor = (AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right);
+            listBoxes.Last().SelectedIndexChanged += new System.EventHandler(this.listBoxes_SelectedIndexChanged);
+
+            // Datagrid for properties
+            dataGridViews[BOTTOM_PROPERTIES].Last().TabIndex = 3;
+            dataGridViews[BOTTOM_PROPERTIES].Last().Dock = DockStyle.Fill;
+            dataGridViews[BOTTOM_PROPERTIES].Last().ColumnCount = 2;
+            dataGridViews[BOTTOM_PROPERTIES].Last().Columns[0].Name = "Item";
+            dataGridViews[BOTTOM_PROPERTIES].Last().Columns[1].Name = "Value";
+            dataGridViews[BOTTOM_PROPERTIES].Last().SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dataGridViews[BOTTOM_PROPERTIES].Last().AllowUserToAddRows = false;
+            dataGridViews[BOTTOM_PROPERTIES].Last().AllowUserToDeleteRows = false;
+            dataGridViews[BOTTOM_PROPERTIES].Last().AllowUserToOrderColumns = false;
+            dataGridViews[BOTTOM_PROPERTIES].Last().AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViews[BOTTOM_PROPERTIES].Last().RowHeadersVisible = false;
+            dataGridViews[BOTTOM_PROPERTIES].Last().Columns[0].ReadOnly = true;
+            dataGridViews[BOTTOM_PROPERTIES].Last().Enabled = false;
+
+            // Static Datagrid for static properties
+            dataGridViews[TOP_PROPERTIES].Last().TabStop = false;
+            dataGridViews[TOP_PROPERTIES].Last().Dock = DockStyle.Fill;
+            dataGridViews[TOP_PROPERTIES].Last().ColumnCount = 2;
+            dataGridViews[TOP_PROPERTIES].Last().Columns[0].Name = "Item";
+            dataGridViews[TOP_PROPERTIES].Last().Columns[1].Name = "Value";
+            dataGridViews[TOP_PROPERTIES].Last().AllowUserToAddRows = false;
+            dataGridViews[TOP_PROPERTIES].Last().AllowUserToDeleteRows = false;
+            dataGridViews[TOP_PROPERTIES].Last().AllowUserToOrderColumns = false;
+            dataGridViews[TOP_PROPERTIES].Last().AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViews[TOP_PROPERTIES].Last().RowHeadersVisible = false;
+            dataGridViews[TOP_PROPERTIES].Last().ReadOnly = true;
+
+
+            // Label for ID
+            labels.Last().Location = new System.Drawing.Point(PANEL_PADDING, PANEL_PADDING);
+            labels.Last().Size = new System.Drawing.Size(900, 42);
+            labels.Last().Font = fntID;
+            labels.Last().Text = "No ID Selected";
+
+            //Panel
+
+            // Add elements
+            tbSelectionTabs.TabPages[tbSelectionTabs.TabPages.Count - 1].Controls.Add(splitContainers[MAIN_SPLIT].Last());
+            splitContainers[MAIN_SPLIT].Last().Panel2.Controls.Add(splitContainers[MAIN_PANEL_2_SPLIT].Last());
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().Panel2.Controls.Add(splitContainers[PROP_SPLIT].Last());
+            splitContainers[MAIN_SPLIT].Last().Panel1.Controls.Add(splitContainers[MAIN_PANEL_1_SPLIT].Last());
+            splitContainers[MAIN_PANEL_1_SPLIT].Last().Panel1.Controls.Add(comboBoxes.Last());
+            splitContainers[MAIN_PANEL_1_SPLIT].Last().Panel2.Controls.Add(listBoxes.Last());
+            splitContainers[PROP_SPLIT].Last().Panel2.Controls.Add(dataGridViews[BOTTOM_PROPERTIES].Last());
+            splitContainers[PROP_SPLIT].Last().Panel1.Controls.Add(dataGridViews[TOP_PROPERTIES].Last());
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().Panel1.Controls.Add(labels.Last());
+
+            /*Unfortunately there is an odd quirk with controls that are Dockstyle.Fill within a
+            splitter panel.  As a result I have to change the splitter distance after the controls
+            are added to the form.  This has the disadvantage of showing the redraw for a split second
+            But is was the best option I could come up with.  Otherwise it would have made some odd 
+            assumptions about the drawing space and we'd have a very tiny pane on the left.*/
+
+            splitContainers[MAIN_SPLIT].Last().SplitterDistance = MAIN_SPLITTER_DISTANCE;
+            splitContainers[MAIN_PANEL_1_SPLIT].Last().SplitterDistance = MAIN_PANEL1_SPLITTER_DISTANCE;
+            splitContainers[MAIN_PANEL_2_SPLIT].Last().SplitterDistance = MAIN_PANEL2_SPLITTER_DISTANCE;
+            listBoxes.Last().Size = new Size(splitContainers[MAIN_PANEL_1_SPLIT].Last().Panel2.Width, splitContainers[MAIN_PANEL_2_SPLIT].Last().Panel2.Height);
+            splitContainers[PROP_SPLIT].Last().SplitterDistance = DETAILS_PROPERTIES_SPLITTER_DISTANCE;
+        }
+        /// <summary>
+        /// This takes a series of strings and adds them to the listbox that will be used to select the different
+        /// items to view.  
+        /// </summary>
+        /// <param name="tabIndex">Int representing which tab's listbox to add the items</param>
+        /// <param name="values">An array of strings to add to the listbox</param>
+        public void UpdateSelections(int tabIndex, string[] values)
+        {
+            if (tabIndex < tbSelectionTabs.TabCount)
+            {
+                string valuePrefix;
+                // Clear items from selection listbox and repopulate with the values sent
+                listBoxes[tabIndex].Items.Clear();
+                foreach (string value in values)
+                {
+                    listBoxes[tabIndex].Items.Add(value);
+                    valuePrefix = value.Substring(0, 5); 
+                    if (!comboBoxes[tabIndex].Items.Contains(valuePrefix))
+                    {
+                        comboBoxes[tabIndex].Items.Add(valuePrefix);
+                    }
+                }
+            }
+            else
+                MessageBox.Show("UpdateListBox method called with a tab that does not exist!",
+                    "Invalid Tab Reference", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+        }
+        private void listBoxes_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            //Get the currently selected item in the ListBox.
+            string curItem = listBoxes[tbSelectionTabs.SelectedIndex].SelectedItem.ToString();
+            MessageBox.Show("Event triggered.");
+
+        }
+
+        /// <summary>
+        /// This method takes a dictionary and displays the contents in the datagridview window of the user's chosing.
+        /// </summary>
+        /// <param name="tabIndex">Int representing which tab's datagridview to add the dictionary</param>
+        /// <param name="gridIndex">Int representing which datagrid to take the datagrid</param>
+        /// <param name="propertiesDict">A dictionary of strings to display in the datagridview</param>
+        public void UpdatePropertiesGrid(int tabIndex, int gridIndex, Dictionary<string, string> propertiesDict)
+        {
+            //here you'll need to determine which grid in the array to add to then just go through the dictionary
+            if (tabIndex < tbSelectionTabs.TabCount)
+            {
+                if (gridIndex < dataGridViews.Count())
+                {
+                    foreach (var item in propertiesDict)
+                    {
+                        dataGridViews[gridIndex].ElementAt(tabIndex).Rows.Add(item);
+                    }
+                }
+                else
+                    MessageBox.Show("UpdatePropertiesGrid method called with a datagrid that does not exist!",
+                    "Invalid Data Grid Reference", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            else
+                MessageBox.Show("UpdatePropertiesGrid method called with a tab that does not exist!",
+                    "Invalid Tab Reference", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+        }
+
+        public Dictionary<string,string> GetPropertiesGridData(int tabIndex, int gridIndex)
+        {
+            Dictionary<string, string> returnValues = new Dictionary<string, string>();
+            if (tabIndex< tbSelectionTabs.TabCount)
+            {
+                if (gridIndex < dataGridViews.Count())
+                {
+                    for (int i = 0; i < dataGridViews[tabIndex].ElementAt(gridIndex).Rows.Count; i++)
+                    {
+                        returnValues.Add(dataGridViews[tabIndex].ElementAt(gridIndex).Rows[i].Cells[0].Value.ToString(),
+                            dataGridViews[tabIndex].ElementAt(gridIndex).Rows[i].Cells[1].Value.ToString());
+                    }
+                }
+                 else
+                    MessageBox.Show("GetPropertiesGridData method called with a datagrid that does not exist!",
+                    "Invalid Data Grid Reference", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+             else
+                MessageBox.Show("GetPropertiesGridData method called with a tab that does not exist!",
+                    "Invalid Tab Reference", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+          return returnValues;
+        }
+        public void AddNotesField(int tabIndex)
+        {
+            if(tabIndex< tbSelectionTabs.TabCount)
+            {
+                
+            }
+              else
+                MessageBox.Show("AddNotesField method called with a tab that does not exist!",
+                    "Invalid Tab Reference", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+       }
+        /*
+        public string[] GetNotesFields(int tabIndex)
+        public void AddDisplayField(int tabIndex)
+        public void AddButton(int tabIndex)
+        public void AddSelectionBox(int tabIndex)
+        public void Print()
+         */
+    }
+}
---
+++ EngineeringGUI/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineeringGUI
+{
+    static class Test
+    {
+        static MainForm gui = Program.main;
+        public static void UserSettings()
+        {
+            List<string> listOfStrings = new List<string>();
+            for (int i =0; i < 10; i++)
+            {
+                listOfStrings.Add("Item " + i);
+            }
+            gui.AddNewTab();
+            gui.UpdateSelections(0, listOfStrings.ToArray());
+        }
+    }
+}
